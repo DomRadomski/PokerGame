a4012383d5fe4de3bf9a0672712c2f93e29ccdcc
DomRadomski <domradomski123@gmail.com>
2025-02-08 20:31:23 +0000

    card class: all tests passed

--- src/main/java/Card.java
+++ src/main/java/Card.java
@@ -1,2 +1,26 @@
-public class Card {
+public class Card
+{
+    private final String rank;
+    private final String suit;
+
+    public Card(String inputRank, String inputSuit)
+    {
+        this.rank = inputRank;
+        this.suit = inputSuit;
+    }
+
+    public String getRank()
+    {
+        return this.rank;
+    }
+
+    public String getSuit()
+    {
+        return this.suit;
+    }
+
+    public String toString()
+    {
+        return getRank() + " of " + getSuit();
+    }
 }
