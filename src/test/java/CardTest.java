1f22c09037455b41c24f2545232c768f27abff6c
DomRadomski <domradomski123@gmail.com>
2025-02-08 20:13:52 +0000

    card tests

--- src/test/java/CardTest.java
+++ src/test/java/CardTest.java
@@ -1,2 +1,22 @@
-public class CardTest {
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CardTest {
+
+    @Test
+    void cardStoresRankAndSuit()
+    {
+        Card card = new Card("Ace", "Hearts");
+        assertEquals("Ace", card.getRank());
+        assertEquals("Hearts", card.getSuit());
+    }
+
+    @Test
+    void cardToStringReturnsCorrectFormat()
+    {
+        Card card = new Card("King", "Spades");
+        assertEquals("King of Spades", card.toString());
+    }
 }
+
